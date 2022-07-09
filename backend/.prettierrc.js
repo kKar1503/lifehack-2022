c26d1f912141e1305210bf57ffdf17356cb1dbf8
kKar1503 <isonice4572@gmail.com>
2022-07-09 10:09:11 +0800

    ci: :wrench: Added Prettier and ESLint config

---
+++ backend/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	bracketSpacing: true,
+	bracketSameLine: false,
+	singleQuote: false,
+	trailingComma: "all",
+	arrowParens: "avoid",
+	semi: true,
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 100,
+	endOfLine: "lf",
+	singleAttributePerLine: true,
+};
