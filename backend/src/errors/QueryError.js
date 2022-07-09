52b011c17f73a3fe28bd3dbf51921a7044a939bb
kKar1503 <isonice4572@gmail.com>
2022-07-09 20:40:05 +0800

    feat: :sparkles: Custom errors

---
+++ backend/src/errors/QueryError.js
@@ -0,0 +1,51 @@
+class QueryError extends Error {
+	constructor() {
+		super();
+		this.name = 'QueryError';
+		this.message = 'Invalid query';
+		this.code = 400;
+	}
+}
+
+class ParamTypeError extends QueryError {
+	constructor(paramName, paramValue, expectedValue) {
+		super();
+		this.name = 'ParamTypeError';
+		this.message = `Parameter '${paramName}' expected a ${Object.prototype.toString.call(
+			expectedValue,
+		)} but got a ${Object.prototype.toString.call(paramValue)}`;
+	}
+}
+
+class ParamMissingError extends QueryError {
+	constructor(paramName) {
+		super();
+		this.name = 'ParamMissingError';
+		this.message = `Parameter '${paramName}' is missing`;
+	}
+}
+
+class ValidationError extends QueryError {
+	constructor() {
+		super();
+		this.name = 'ValidationError';
+		this.message = 'Invalid data';
+	}
+}
+
+class NotFoundError extends QueryError {
+	constructor(item) {
+		super();
+		this.name = 'NotFoundError';
+		this.message = `Cannot find ${item}`;
+		this.code = 404;
+	}
+}
+
+module.exports = {
+	QueryError,
+	NotFoundError,
+	ValidationError,
+	ParamMissingError,
+	ParamTypeError,
+};
