e66b525a725dcb1c975c2ae293a1aa3f09750e1f
Malcolm Sng <malcolmsng99@gmail.com>
2022-07-09 15:33:08 +0800

    navbar + login register

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,8 +1,20 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
+import { HomeNav } from "../components/homeNav.jsx";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Home() {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+  <div id = "homepage">
+<HomeNav></HomeNav>
+{/* <Button variant="contained">Hello World</Button> */}
+</div>
+);
 }
