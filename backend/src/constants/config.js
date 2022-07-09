d86398816ed05eb0dd8d3512292d8e464baa4d35
kKar1503 <isonice4572@gmail.com>
2022-07-09 14:02:17 +0800

    feat: :sparkles: Added constants for firebase

--- backend/src/constants/config.js
+++ backend/src/constants/config.js
@@ -3,6 +3,15 @@ require('dotenv').config();
 const CONFIG = {
 	PORT: process.env.PORT,
 	NODE_ENV: process.env.NODE_ENV,
+	FIREBASE_CONFIG: {
+		apiKey: process.env.API_KEY,
+		authDomain: process.env.AUTH_DOMAIN,
+		projectId: process.env.PROJECT_ID,
+		storageBucket: process.env.STORAGE_BUCKET,
+		messagingSenderId: process.env.MESSAGING_SENDER_ID,
+		appId: process.env.APP_ID,
+		measurementId: process.env.MEASUREMENT_ID,
+	},
 };
 
 module.exports = CONFIG;
