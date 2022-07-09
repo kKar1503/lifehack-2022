fa8f020d95f10e71a60254f4afcd76af4d350e39
kKar1503 <isonice4572@gmail.com>
2022-07-09 10:12:53 +0800

    feat: :sparkles: Added minor change to prettier config

--- backend/.prettierrc.js
+++ backend/.prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
 	bracketSpacing: true,
 	bracketSameLine: false,
-	singleQuote: false,
-	trailingComma: "all",
-	arrowParens: "avoid",
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'avoid',
 	semi: true,
 	tabWidth: 4,
 	useTabs: true,
-	printWidth: 100,
-	endOfLine: "lf",
+	printWidth: 80,
+	endOfLine: 'lf',
 	singleAttributePerLine: true,
 };
