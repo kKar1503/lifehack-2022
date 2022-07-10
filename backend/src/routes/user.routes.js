6fbbb32417e5c30c41d4f9f715f2a6b39aadef81
kKar1503 <isonice4572@gmail.com>
2022-07-10 09:05:26 +0800

    fix: :bug: fixed auth

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const { addUser, getUserById } = require('../controllers').User;
 const { authentication } = require('../middlewares');
 
-router.use(authentication);
-
-router.post('/signup', addUser);
-router.get('/user', getUserById);
+router.post('/signup', authentication, addUser);
+router.get('/user', authentication, getUserById);
 
 module.exports = router;
